02c02822fd8fb62e3f6f4c397b62becc6688898f
Anda <andzelikawos56@gmail.com>
2023-09-05 15:05:42 +0200

    array values using a for loop

---
+++ Task 2/array, loop and switch-case.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int platRR[] = { 1,15,30,59,85,99 };
+	for (int i = 0; i < 6; i++) 
+	{
+		cout << platRR[i] << "\n";
+	}
+	return 0;
+}
